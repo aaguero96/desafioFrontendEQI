02e223a7b42b597c64d308f40970fbb4557da2c9
aaguero96 <andre.luiz_1996@hotmail.com>
2022-02-07 15:35:47 -0300

    Create a generic structure to component

---
+++ desafio-frontend/src/Components/InputCurrency.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+// import { connect } from 'react-redux';
+
+class InputCurrency extends React.Component {
+  render() {
+    return (
+      <div>
+      </div>
+    )
+  }
+}
+
+// const mapStateToProps = (state) => ({});
+// const mapDispatchToProps = (dispatch) => ({})
+// export default connect(mapStateToProps, mapDispatchToProps)(InputCurrency);
+
+export default InputCurrency;
