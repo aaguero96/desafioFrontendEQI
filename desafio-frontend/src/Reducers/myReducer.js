2de94ebd46bf87b16474944b54ee9f85bd87a8fc
aaguero96 <andre.luiz_1996@hotmail.com>
2022-02-07 15:51:51 -0300

    Add case CHANGE_INCONTRIBUITION in myReducer, and state inContribuition

--- desafio-frontend/src/Reducers/myReducer.js
+++ desafio-frontend/src/Reducers/myReducer.js
@@ -1,9 +1,14 @@
 const INITIAL_STATE = {
-
+  inContribuition: '',
 };
 
 export default function myReducer(state = INITIAL_STATE, action) {
   switch (action.type) {
+    case 'CHANGE_INCONTRIBUITION':
+      return {
+        ...state,
+        inContribuition: action.newInContribuition,
+      }
     default:
       return state;
   }
