2620f6f8e9d60152514b869856e33c4d69812a64
aaguero96 <andre.luiz_1996@hotmail.com>
2022-02-08 11:09:24 -0300

    Render buttons

--- desafio-frontend/src/Components/InputIncome.jsx
+++ desafio-frontend/src/Components/InputIncome.jsx
@@ -1,13 +1,76 @@
 import React from 'react';
+import { getSimulationFromAPI } from '../Functions/functions';
+import '../CSS/InputIncome.css';
 
 class InputIncome extends React.Component {
   constructor() {
     super();
+    this.state = {
+      incomeList: [],
+      loadingList: true,
+      selectedIncome: 'bruto',
+    };
   };
+
+  requireSimulation = async () => {
+    const simulationData = await getSimulationFromAPI();
+    this.setState({
+      loadingList: false,
+      incomeList: simulationData
+        .map((element) => element.tipoRendimento)
+        .filter((element, index, arr) => arr.indexOf(element) === index),
+    });
+  };
+
+  correctClass = (element) => {
+    const { selectedIncome } = this.state;
+    const NON_SELECTED_CLASS = 'non-selected';
+    const SELECTED_CLASS = 'selected';
+    if (selectedIncome === element) {
+      return SELECTED_CLASS;
+    }
+    return NON_SELECTED_CLASS;
+  };
+
+  changeSelected = ({ target: { name } }) => {
+    console.log(name);
+    this.setState({
+      selectedIncome: name,
+    });
+  };
+
+  renderButtons = () => {
+    const { incomeList, loadingList } = this.state;
+    if (loadingList) {
+      return (
+        <span>Loading ...</span>
+      )
+    }
+    return (
+      incomeList.map((element) => (
+        <button
+          key={ `Rendimento: ${element}` }
+          type="button"
+          name={ element }
+          className={ this.correctClass(element) }
+          onClick={ this.changeSelected }
+        >
+          {element}
+        </button>
+      ))
+    )
+  };
+
+  componentDidMount() {
+    this.requireSimulation();
+  }
 
   render() {
     return (
-      <h3>Rendimento</h3>
+      <div>
+        <h3>Rendimento</h3>
+        { this.renderButtons() }
+      </div>
     )
   }
 };
