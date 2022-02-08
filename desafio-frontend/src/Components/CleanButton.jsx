0bbb320213347f50a0c989be7f87a64ded53e629
aaguero96 <andre.luiz_1996@hotmail.com>
2022-02-08 11:43:43 -0300

    Create general structure to this component

---
+++ desafio-frontend/src/Components/CleanButton.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class CleanButton extends React.Component {
+  constructor() {
+    super();
+  };
+
+  render() {
+    const { name, changePercentageState, percentageState } = this.props;
+    return (
+      <button>
+        Limpar Campos
+      </button>
+    )
+  }
+};
+
+export default CleanButton;
