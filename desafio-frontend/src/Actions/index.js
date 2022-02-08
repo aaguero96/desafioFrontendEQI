7a7503a927802399ebfceba145653a845b6b560c
aaguero96 <andre.luiz_1996@hotmail.com>
2022-02-08 09:28:51 -0300

    Create action to change Percentage state

--- desafio-frontend/src/Actions/index.js
+++ desafio-frontend/src/Actions/index.js
@@ -1,5 +1,11 @@
-export const changeCurrencyStateAction= (key, newCurrencyState) => ({
+export const changeCurrencyStateAction = (key, newCurrencyState) => ({
   type: 'CHANGE_CURRENCY_STATE',
   newCurrencyState,
+  key,
+});
+
+export const changePercentageStateAction = (key, newPercentageState) => ({
+  type: 'CHANGE_PERCENTAGE_STATE',
+  newPercentageState,
   key,
 });
