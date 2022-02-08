fbcee0d5e45db5809956dbdf59678f3c0b7e66d9
aaguero96 <andre.luiz_1996@hotmail.com>
2022-02-08 09:59:10 -0300

    Create first structure to this component

---
+++ desafio-frontend/src/Components/InputIncome.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class InputIncome extends React.Component {
+  constructor() {
+    super();
+  };
+
+  render() {
+    return (
+      <h1>Rendimento</h1>
+    )
+  }
+};
+
+export default InputIncome;
