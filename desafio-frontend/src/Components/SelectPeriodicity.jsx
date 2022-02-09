44e167f3b20698bf3903f8a438cc2eb68d2a2537
aaguero96 <andre.luiz_1996@hotmail.com>
2022-02-09 12:01:42 -0300

    finish logical part in selectPeriodicity component

--- desafio-frontend/src/Components/SelectPeriodicity.jsx
+++ desafio-frontend/src/Components/SelectPeriodicity.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { changePeriodicityAction } from '../Actions';
 
 class SelectPeriodicity extends React.Component {
   constructor() {
@@ -12,15 +13,20 @@ class SelectPeriodicity extends React.Component {
   };
 
   nameToKey = {
-    'Prazo': '',
+    'Prazo': 'deadLinePeriodicity',
   }
 
   render() {
+    const { periodicity, name, changePeriodicity } = this.props;
+    const selectedElement = periodicity[this.nameToKey[name]];
     return (
-      <select>
+      <select
+        defaultValue={ selectedElement }
+        onChange={ ({ target: { value } }) => changePeriodicity(this.nameToKey[name], value) }
+      >
         {
           Object.keys(this.option).map((element) => (
-            <option value={ element }>
+            <option key={ `${name}-${element}` } value={ element }>
               { element }
             </option>
           ))
@@ -30,12 +36,10 @@ class SelectPeriodicity extends React.Component {
   }
 };
 
-// const mapStateToProps = (state) => ({
-//   percentageState: state.myReducer,
-// });
-// const mapDispatchToProps = (dispatch) => ({
-//   changePercentageState: (state) => dispatch(changePercentageStateAction(state)),
-// });
-// export default connect(mapStateToProps, mapDispatchToProps)(SelectPeriodicity);
-
-export default SelectPeriodicity;
+const mapStateToProps = (state) => ({
+  periodicity: state.myReducer,
+});
+const mapDispatchToProps = (dispatch) => ({
+  changePeriodicity: (key, state) => dispatch(changePeriodicityAction(key, state)),
+});
+export default connect(mapStateToProps, mapDispatchToProps)(SelectPeriodicity);
