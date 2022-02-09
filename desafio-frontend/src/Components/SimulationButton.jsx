a4cc171d652f2f95bc6cc0262a7aefdad7cdbbc1
aaguero96 <andre.luiz_1996@hotmail.com>
2022-02-09 14:52:57 -0300

    Add validation functions

--- desafio-frontend/src/Components/SimulationButton.jsx
+++ desafio-frontend/src/Components/SimulationButton.jsx
@@ -1,20 +1,76 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import {
+  validationCurrencies,
+  validationInteger,
+  validationPercentage,
+} from '../Functions/functions';
 
 class SimulationButton extends React.Component {
   constructor() {
     super();
   };
 
-  render() {
+  validationFill = () => {
     const { myReducer: {
       inContribuition,
+      mensalContribuition,
+      profitability,
+      ipca,
+      cdi,
+      deadlines,
+    } } = this.props;
+    let validation = validationCurrencies(inContribuition);
+    validation &&= validationCurrencies(mensalContribuition);
+    validation &&= validationPercentage(profitability);
+    validation &&= validationPercentage(ipca);
+    validation &&= validationPercentage(cdi);
+    validation &&= validationInteger(deadlines);
+    return validation;
+  };
+
+  showParameters = () => {
+    const { myReducer: {
+      inContribuition,
+      mensalContribuition,
+      profitability,
+      profitabilityFreq,
+      ipca,
+      ipcaFreq,
+      cdi,
+      cdiFreq,
     } } = this.props;
     return (
-      <div>
+      <>
         <div>
           { `Contribuição Inicial: R$ ${inContribuition}` }
         </div>
+        <div>
+          { `Aporte Mensal: R$ ${mensalContribuition}` }
+        </div>
+        <div>
+          { `Rentabilidade ${profitabilityFreq}: ${profitability} %` }
+        </div>
+        <div>
+          { `IPCA ${ipcaFreq}: ${ipca} %` }
+        </div>
+        <div>
+          { `CDI ${cdiFreq}: ${cdi} %` }
+        </div>
+      </>
+    )
+  }
+
+  render() {
+    return (
+      <div>
+        {
+          this.validationFill() ? (
+            this.showParameters()
+          ) : (
+            <span>Preencha os campos ao lado</span>
+          )
+        }
       </div>
     )
   }
