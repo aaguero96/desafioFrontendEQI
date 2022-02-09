68df76536e7dde6cc5b9b031f56c75866b9ded54
aaguero96 <andre.luiz_1996@hotmail.com>
2022-02-09 15:27:33 -0300

    Add message part to indexingMessage

---
+++ desafio-frontend/src/Components/IndexingMessage.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import '../CSS/InputIndexing.css';
+import { connect } from 'react-redux';
+
+class IndexingMessage extends React.Component {
+  constructor() {
+    super();
+  };
+
+  message = () => {
+    const { indexing } = this.props;
+    if (indexing === 'pre') return 'Juros total no periodo';
+    if (indexing === 'pos') return 'Juros acrescidos do CDI: e.g.:(1+J%).CDI';
+    return 'Juros acrescidos do IPCA: e.g.:J% + CDI';
+  };
+
+  render() {
+    return (
+      <span>{ this.message() }</span>
+    )
+  }
+};
+
+const mapStateToProps = (state) => ({
+  indexing: state.myReducer.indexing,
+});
+export default connect(mapStateToProps)(IndexingMessage);
