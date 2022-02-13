423b130c515f5e6da358403e09d3e5e9b128e811
aaguero96 <andre.luiz_1996@hotmail.com>
2022-02-12 21:04:51 -0300

    Rearrange divs in app

--- desafio-frontend/src/App.js
+++ desafio-frontend/src/App.js
@@ -21,57 +21,53 @@ function App() {
   return (
     <>
       <div className="header">
-        <CleanButton />
         <h1>Simulador de Investimentos</h1>
         <img className="logo" src={ logoURL }/>
       </div>
       <hr className="barra-divisoria"/>
+      <CleanButton />
       <div className="container-principal">
         <div className="container-formulario">
-          <div className="container-selectors">
+          <div className="container-part1">
             <InputIncome />
             <div>
-              <InputIndexing />
-              <IndexingMessage />
+              <InputCurrency name={ "Aporte Inicial" }/>
+              <ValidationMessage name={ "Aporte Inicial" } validationFunction={ validationCurrencies }/>
+            </div>
+            <div>
+              <InputCurrency name={ "Aporte Mensal" }/>
+              <ValidationMessage name={ "Aporte Mensal" } validationFunction={ validationCurrencies }/>
+            </div>
+            <div>
+              <InputInteger name={ "Prazo" }/>
+              <SelectPeriodicity name={ "Prazo" } />
+              <ValidationMessage name={ "Prazo" } validationFunction={ validationInteger }/>
             </div>
           </div>
-          <div className="container-inputs">
-            <div className="container-valores">
-              <div>
-                <InputCurrency name={ "Aporte Inicial" }/>
-                <ValidationMessage name={ "Aporte Inicial" } validationFunction={ validationCurrencies }/>
-              </div>
-              <div>
-                <InputCurrency name={ "Aporte Mensal" }/>
-                <ValidationMessage name={ "Aporte Mensal" } validationFunction={ validationCurrencies }/>
-              </div>
-              <div>
-                <InputInteger name={ "Prazo" }/>
-                <SelectPeriodicity name={ "Prazo" } />
-                <ValidationMessage name={ "Prazo" } validationFunction={ validationInteger }/>
-              </div> 
+          <div className="container-part2">
+            <div>
+                <InputIndexing />
+                <IndexingMessage />
             </div>
-            <div className="container-porcentagens">
-                <div>
-                  <InputPercentage name={ "Rentabilidade" }/>
-                  <SelectFrequency name ={ "Rentabilidade" }/>
-                  <ValidationMessage name={ "Rentabilidade" } validationFunction={ validationPercentage }/>
-                </div>
-                <div>
-                  <InputPercentage name={ "IPCA" }/>
-                  <SelectFrequency name ={ "IPCA" }/>
-                  <ValidationMessage name={ "IPCA" } validationFunction={ validationPercentage }/>
-                </div>
-                <div>
-                  <InputPercentage name={ "CDI" }/>
-                  <SelectFrequency name ={ "CDI" }/>
-                  <ValidationMessage name={ "CDI" } validationFunction={ validationPercentage }/>
-                </div> 
+            <div>
+              <InputPercentage name={ "Rentabilidade" }/>
+              <SelectFrequency name ={ "Rentabilidade" }/>
+              <ValidationMessage name={ "Rentabilidade" } validationFunction={ validationPercentage }/>
             </div>
+            <div>
+              <InputPercentage name={ "IPCA" }/>
+              <SelectFrequency name ={ "IPCA" }/>
+              <ValidationMessage name={ "IPCA" } validationFunction={ validationPercentage }/>
+            </div>
+            <div>
+              <InputPercentage name={ "CDI" }/>
+              <SelectFrequency name ={ "CDI" }/>
+              <ValidationMessage name={ "CDI" } validationFunction={ validationPercentage }/>
+            </div> 
           </div>
         </div>
-        <div container-indicadores>
-          <Simulation />
+        <div className="container-indicadores">
+        <Simulation />
         </div>
       </div>
     </>
