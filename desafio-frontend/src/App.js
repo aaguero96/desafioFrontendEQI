fbf57fce503345f86e1f3fbbf05f0b3495b8684a
aaguero96 <andre.luiz_1996@hotmail.com>
2022-02-14 20:36:59 -0300

    Add form component

--- desafio-frontend/src/App.js
+++ desafio-frontend/src/App.js
@@ -1,19 +1,8 @@
-import InputCurrency from './Components/InputCurrency';
-import InputPercentage from './Components/InputPercentage';
-import InputInteger from './Components/InputInteger'
-import InputIncome from './Components/InputIncome';
-import InputIndexing from './Components/InputIndexing';
+
 import CleanButton from './Components/CleanButton';
-import SelectFrequency from './Components/SelectFrequency';
-import SelectPeriodicity from './Components/SelectPeriodicity';
+
 import Simulation from './Components/Simulation';
-import IndexingMessage from './Components/IndexingMessage';
-import ValidationMessage from './Components/ValidationMessage';
-import {
-  validationCurrencies,
-  validationInteger,
-  validationPercentage,
-} from './Functions/functions';
+import Form from './Components/Form';
 import './CSS/App.css';
 import { logoURL } from './Images/logo.js';
 
@@ -27,45 +16,7 @@ function App() {
       <hr className="barra-divisoria"/>
       <CleanButton />
       <div className="container-principal">
-        <div className="container-formulario">
-          <div className="container-part1">
-            <InputIncome />
-            <div>
-              <InputCurrency name={ "Aporte Inicial" }/>
-              <ValidationMessage name={ "Aporte Inicial" } validationFunction={ validationCurrencies }/>
-            </div>
-            <div>
-              <InputCurrency name={ "Aporte Mensal" }/>
-              <ValidationMessage name={ "Aporte Mensal" } validationFunction={ validationCurrencies }/>
-            </div>
-            <div>
-              <InputInteger name={ "Prazo" }/>
-              <SelectPeriodicity name={ "Prazo" } />
-              <ValidationMessage name={ "Prazo" } validationFunction={ validationInteger }/>
-            </div>
-          </div>
-          <div className="container-part2">
-            <div>
-                <InputIndexing />
-                <IndexingMessage />
-            </div>
-            <div>
-              <InputPercentage name={ "Rentabilidade" }/>
-              <SelectFrequency name ={ "Rentabilidade" }/>
-              <ValidationMessage name={ "Rentabilidade" } validationFunction={ validationPercentage }/>
-            </div>
-            <div>
-              <InputPercentage name={ "IPCA" }/>
-              <SelectFrequency name ={ "IPCA" }/>
-              <ValidationMessage name={ "IPCA" } validationFunction={ validationPercentage }/>
-            </div>
-            <div>
-              <InputPercentage name={ "CDI" }/>
-              <SelectFrequency name ={ "CDI" }/>
-              <ValidationMessage name={ "CDI" } validationFunction={ validationPercentage }/>
-            </div> 
-          </div>
-        </div>
+        <Form />
         <div className="container-indicadores">
         <Simulation />
         </div>
---
+++ desafio-frontend/src/Components/Form.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import InputCurrency from './InputCurrency';
+import InputPercentage from './InputPercentage';
+import InputInteger from './InputInteger'
+import InputIncome from './InputIncome';
+import InputIndexing from './InputIndexing';
+import SelectFrequency from './SelectFrequency';
+import SelectPeriodicity from './SelectPeriodicity';
+import IndexingMessage from './IndexingMessage';
+import ValidationMessage from './ValidationMessage';
+import {
+  validationCurrencies,
+  validationInteger,
+  validationPercentage,
+} from '../Functions/functions';
+
+class Form extends React.Component {
+  constructor() {
+    super();
+  };
+
+  render() {
+    return ( 
+      <div className="container-formulario">
+        <div className="container-part1">
+          <InputIncome />
+          <div>
+            <InputCurrency name={ "Aporte Inicial" }/>
+            <ValidationMessage name={ "Aporte Inicial" } validationFunction={ validationCurrencies }/>
+          </div>
+          <div>
+            <InputCurrency name={ "Aporte Mensal" }/>
+            <ValidationMessage name={ "Aporte Mensal" } validationFunction={ validationCurrencies }/>
+          </div>
+          <div>
+            <InputInteger name={ "Prazo" }/>
+            <SelectPeriodicity name={ "Prazo" } />
+            <ValidationMessage name={ "Prazo" } validationFunction={ validationInteger }/>
+          </div>
+        </div>
+        <div className="container-part2">
+          <div>
+              <InputIndexing />
+              <IndexingMessage />
+          </div>
+          <div>
+            <InputPercentage name={ "Rentabilidade" }/>
+            <SelectFrequency name ={ "Rentabilidade" }/>
+            <ValidationMessage name={ "Rentabilidade" } validationFunction={ validationPercentage }/>
+          </div>
+          <div>
+            <InputPercentage name={ "IPCA" }/>
+            <SelectFrequency name ={ "IPCA" }/>
+            <ValidationMessage name={ "IPCA" } validationFunction={ validationPercentage }/>
+          </div>
+          <div>
+            <InputPercentage name={ "CDI" }/>
+            <SelectFrequency name ={ "CDI" }/>
+            <ValidationMessage name={ "CDI" } validationFunction={ validationPercentage }/>
+          </div> 
+        </div>
+      </div>
+    )
+  }
+};
+
+export default Form;
