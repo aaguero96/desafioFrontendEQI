46e4a6fe17145e85b4ed855852ab216c7775caa1
aaguero96 <andre.luiz_1996@hotmail.com>
2022-02-14 23:05:16 -0300

    Rearrange App divs and add Form

--- desafio-frontend/src/App.js
+++ desafio-frontend/src/App.js
@@ -1,28 +1,29 @@
-
+import React from 'react';
 import CleanButton from './Components/CleanButton';
-
 import Simulation from './Components/Simulation';
 import Form from './Components/Form';
 import './CSS/App.css';
 import { logoURL } from './Images/logo.js';
 
-function App() {
-  return (
-    <>
-      <div className="header">
-        <h1>Simulador de Investimentos</h1>
-        <img className="logo" src={ logoURL }/>
-      </div>
-      <hr className="barra-divisoria"/>
-      <CleanButton />
-      <div className="container-principal">
-        <Form />
-        <div className="container-indicadores">
-        <Simulation />
+class App extends React.Component {
+  render () {
+    return (
+      <>
+        <div className="header">
+          <h1>Simulador de Investimentos</h1>
+          <img className="logo" src={ logoURL }/>
         </div>
-      </div>
-    </>
-  );
+        <hr className="barra-divisoria"/>
+        <CleanButton />
+        <div className="container-principal">
+          <Form />
+          <div className="container-indicadores">
+          <Simulation />
+          </div>
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
